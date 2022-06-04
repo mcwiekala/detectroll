8e77f36ef42f0b96e69cdecff38acb401be1006d
Karol Wolski <karolwolski04@gmail.com>
2022-06-05 01:25:50 +0200

    feat: select

--- client/src/components/TextArea/TextArea.jsx
+++ client/src/components/TextArea/TextArea.jsx
@@ -2,14 +2,23 @@ import styles from './TextArea.module.scss'
 import { IoSend } from 'react-icons/io5'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
+import Select from '../Select/Select'
+import { useState } from 'react'
+
 const TextArea = () => {
   const { register, handleSubmit, watch } = useForm()
+  const [lang, setLang] = useState()
   const searchText = watch('messageText')
+  const selectCallback = (event) => {
+    event.preventDefault()
+    setLang(event.target.value)
+  }
+
   const onSubmit = () => {
     axios
       .post('/api/analyze', {
         text: searchText,
-        lang: 'en',
+        lang,
       })
       .then((response) => console.log(response.data))
       .catch((error) => console.error(error))
@@ -20,6 +29,7 @@ const TextArea = () => {
       <button type="submit" className={styles.sendIconContainer}>
         <IoSend className={styles.sendIcon} />
       </button>
+      <Select onChange={selectCallback} />
     </form>
   )
 }
