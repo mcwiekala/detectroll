7381af4ac4857c467341893cb1de801d37805a8c
TARGEToff <lockontargt@gmail.com>
2022-06-05 01:36:05 +0200

    additional messages

--- server/messages.ts
+++ server/messages.ts
@@ -1,6 +1,7 @@
 import { perspectiveClientResponse } from './perspective/types'
 
 const messageMap: { [key: string]: string } = {
+  '0000': `You're a cool dude!`,
   '1000': 'Do not insult other people!',
   '0100': "You shouldn't curse, children can see it!",
   '0010': 'Threats are criminalised!',
@@ -11,6 +12,11 @@ const messageMap: { [key: string]: string } = {
   '0110': 'Cursing will not make your threats more serious!',
   '0101': 'Work on your own behaviour!',
   '0011': 'Other people may have a worse rest of the day!',
+  '1110': 'All you lack is the toxicity to be an ultimate bad',
+  '0111': 'All you lack is the insulting to be an ultimate bad',
+  '1011': 'All you lack is the cursing to be an ultimate bad',
+  '1101': 'All you lack is the threatening to be an ultimate bad',
+  '1111': `You're just a bad guy.`
 }
 
 const generateMessageCode = (result: perspectiveClientResponse) => {
