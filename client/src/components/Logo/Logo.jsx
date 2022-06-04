8787430f130198e04dff747e1aed89800f47f8c9
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-06-04 22:35:48 +0200

    fix svgs

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,14 +1,14 @@
-import { ReactComponent as LogoSVG } from "./troll3.svg";
-import styles from "./Logo.module.scss";
+import LogoSVG from './troll3.svg'
+import styles from './Logo.module.scss'
 const Logo = () => {
   return (
     <div className={styles.logoContainer}>
-      <LogoSVG className={styles.logo} />
+      <img src={LogoSVG} className={styles.logo} />
       <h1 className={styles.title}>
         <span className={styles.titleColor1}>detec</span>
         <span className={styles.titleColor2}>troll</span>
       </h1>
     </div>
-  );
-};
-export default Logo;
+  )
+}
+export default Logo
