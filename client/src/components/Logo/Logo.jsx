87bd46029129d3ff891dca5e2d9b040e7d23ef50
Karol Wolski <karolwolski04@gmail.com>
2022-06-04 23:33:50 +0200

    link logo

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,14 +1,15 @@
 import LogoSVG from './troll3.svg'
 import styles from './Logo.module.scss'
+import { Link } from 'react-router-dom'
 const Logo = (props) => {
   return (
-    <div {...props} className={`${styles.logoContainer} ${props.className || ''}`}>
-      <img src={LogoSVG} className={styles.logo} />
+    <Link to="/" {...props} className={`${styles.logoContainer} ${props.className || ''}`}>
+      <img src={LogoSVG} className={styles.logo} alt="Logo" />
       <h1 className={styles.title}>
         <span className={styles.titleColor1}>detec</span>
         <span className={styles.titleColor2}>troll</span>
       </h1>
-    </div>
+    </Link>
   )
 }
 export default Logo
