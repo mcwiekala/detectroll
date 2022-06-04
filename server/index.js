a93baa49e1b24d1ab51a7fbe5d3bb95296472f42
TARGEToff <lockontargt@gmail.com>
2022-06-04 14:26:42 +0200

    twitter folder

--- server/index.js
+++ server/index.js
@@ -1,46 +1,17 @@
 import cors from 'cors'
-import path from 'path'
 import dotenv from 'dotenv'
 import express from 'express'
-import fetch from 'node-fetch'
+import { perspective } from './perspective/index.js'
+import { getUser, getUserTweets } from './twitter/index.js'
 
 dotenv.config()
 
 const app = express()
-const token = process.env.BEARER_TOKEN
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const PORT = process.env.PORT || 8000
-const URL = 'https://api.twitter.com/2'
-
-const getUser = async (userName) => {
-  const response = await fetch(`${URL}/users/by/username/${userName}`, {
-    method: 'get',
-    headers: {
-      'User-Agent': 'v2UserLookupJS',
-      authorization: `Bearer ${token}`,
-    },
-  })
-
-  if (response.status === 404) {
-    res.status(404).send('No user found!')
-  }
-
-  return await response.text()
-}
-
-const getUserTweets = async (id) => {
-  const response = await fetch(`${URL}/users/${id}/tweets?tweet.fields=lang&exclude=replies,retweets&`, {
-    method: 'get',
-    headers: {
-      'User-Agent': 'v2UserLookupJS',
-      authorization: `Bearer ${token}`,
-    },
-  })
-  return await response.text()
-}
 
 app.get('/api/analyze/:twitterName', async (req, res) => {
   let user = JSON.parse(await getUser(req.params.twitterName))
