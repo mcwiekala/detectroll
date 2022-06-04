20386b0af817c6f0a315f2141a9bec86760fe108
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-06-04 19:59:49 +0200

    add router (#12)

--- client/src/components/SearchBox/SearchBox.jsx
+++ client/src/components/SearchBox/SearchBox.jsx
@@ -1,27 +1,22 @@
-import styles from "./SearchBox.module.scss";
-import { IoSend } from "react-icons/io5";
-import { useForm, useWatch } from "react-hook-form";
+import styles from './SearchBox.module.scss'
+import { IoSend } from 'react-icons/io5'
+import { useForm, useWatch } from 'react-hook-form'
 const SearchBox = () => {
-  const { register, handleSubmit, watch } = useForm();
-  const searchText = watch("searchBox");
+  const { register, handleSubmit, watch } = useForm()
+  const searchText = watch('searchBox')
   const onSubmit = () => {
-    console.log(searchText);
-  };
+    console.log(searchText)
+  }
   return (
-    <form onSubmit={handleSubmit(onSubmit)} class={styles.searchBoxContainer}>
-      {!searchText && <span class={styles.searchBoxAtIcon}>@</span>}
-      <input
-        type="text"
-        class={styles.searchBox}
-        placeholder="twitter handle..."
-        {...register("searchBox")}
-      ></input>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.searchBoxContainer}>
+      {!searchText && <span className={styles.searchBoxAtIcon}>@</span>}
+      <input type="text" className={styles.searchBox} placeholder="twitter handle..." {...register('searchBox')}></input>
       {searchText && (
-        <button type="submit" class={styles.sendIconContainer}>
-          <IoSend class={styles.sendIcon} />
+        <button type="submit" className={styles.sendIconContainer}>
+          <IoSend className={styles.sendIcon} />
         </button>
       )}
     </form>
-  );
-};
-export default SearchBox;
+  )
+}
+export default SearchBox
