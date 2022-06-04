695d079f0069c7e00e57196cd2ecaaefc98fc9b7
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-06-04 18:57:40 +0200

    Revert "Revert "Merge frontend changes""

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./index.scss";
-import Message from "./components/Message/Message";
-import Logo from "./components/Logo/Logo";
-import SearchBox from "./components/SearchBox/SearchBox";
-import TextArea from "./components/TextArea/TextArea";
 import MainPage from "./pages/MainPage";
 function App() {
   return <MainPage />;
 }
 
-export default App;
+export default App
