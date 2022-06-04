0a8113f40413ebda2c8add486c9f69793c4b32c2
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-06-04 11:10:46 +0200

    front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,17 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CRA with Node on Heroku!
+          Welcome to detecTroll!
           <Message/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
